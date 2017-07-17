117ae569e87a26e34a1fdce0eab7480c5b2a699e
cfh463 <rob.galvin@zebra.com>
2017-07-17 08:43:39 -0400

    updated samples to E4A 6.6

--- SerialCommSample1/app/src/main/java/com/symbol/serialcommsample1/MainActivity.java
+++ SerialCommSample1/app/src/main/java/com/symbol/serialcommsample1/MainActivity.java
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) 2015-2016 Symbol Technologies LLC
+* Copyright (C) 2015-2017 Zebra Technologies Corp
 * All rights reserved.
 */
 package com.symbol.serialcommsample1;
@@ -10,55 +10,89 @@ import com.symbol.emdk.EMDKManager.FEATURE_TYPE;
 import com.symbol.emdk.EMDKResults;
 import com.symbol.emdk.serialcomm.SerialComm;
 import com.symbol.emdk.serialcomm.SerialCommException;
+import com.symbol.emdk.serialcomm.SerialCommManager;
+import com.symbol.emdk.serialcomm.SerialCommResults;
+import com.symbol.emdk.serialcomm.SerialPortInfo;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import java.util.HashMap;
+import java.util.List;
+
 
 
 public class MainActivity extends Activity implements EMDKListener {
 
     private String TAG = MainActivity.class.getSimpleName();
     private EMDKManager emdkManager = null;
-    private SerialComm serialComm = null;
+    private SerialComm serialCommPort = null;
+    private SerialCommManager serialCommManager = null;
+    private EditText txtDataToSend = null;
+    private TextView txtStatus = null;
+    private Button btnRead = null;
+    private Button btnWrite = null;
+    private Spinner spinnerPorts = null;
+    public HashMap<String, SerialPortInfo> supportedPorts = null;
 
-    private EditText editText = null;
-    private TextView statusView = null;
-    private Button readButton = null;
-    private Button writeButton = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editText = (EditText) findViewById(R.id.editText1);
-        editText.setText("Serial Communication Write Data Testing.");
+        txtDataToSend = (EditText) findViewById(R.id.txtDataToSend);
+        txtDataToSend.setText("Serial Communication Write Data Testing.");
 
-        statusView = (TextView) findViewById(R.id.statusView);
-        statusView.setText("");
-        statusView.requestFocus();
+        spinnerPorts = (Spinner)findViewById(R.id.spinnerPorts);
+        btnWrite = (Button) findViewById(R.id.btnWrite);
+        btnRead = (Button) findViewById(R.id.btnRead);
+
+        txtStatus = (TextView) findViewById(R.id.txtStatus);
+        txtStatus.setText("");
+        txtStatus.requestFocus();
 
 
         EMDKResults results = EMDKManager.getEMDKManager(getApplicationContext(), this);
         if (results.statusCode != EMDKResults.STATUS_CODE.SUCCESS) {
-            statusView.setText("Failed to open EMDK");
-        } else {
-            statusView.setText("Opening EMDK...");
+            new AsyncStatusUpdate().execute("EMDKManager object request failed!");
         }
 
-        addReadButtonEvents();
-        writeButtonEvents();
-        setEnabled(false);
+        new AsyncUiControlUpdate().execute(false);
     }
 
+    @Override
+    public void onOpened(EMDKManager emdkManager) {
+        this.emdkManager = emdkManager;
+
+        Log.d(TAG, "EMDK opened");
+
+        try{
+            serialCommManager = (SerialCommManager) this.emdkManager.getInstance(FEATURE_TYPE.SERIALCOMM_EX);
+            if(serialCommManager != null) {
+                populatePorts();
+            }
+            else
+            {
+                new AsyncStatusUpdate().execute(FEATURE_TYPE.SERIALCOMM_EX.toString() +  " Feature not supported.");
+            }
+        }
+        catch(Exception e)
+        {
+            Log.d(TAG, e.getMessage());
+            new AsyncStatusUpdate().execute(e.getMessage());
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -79,9 +113,12 @@ public class MainActivity extends Activity implements EMDKListener {
         return super.onOptionsItemSelected(item);
     }
 
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
+
+        deinitSerialComm();
 
         if (emdkManager != null) {
             emdkManager.release();
@@ -90,47 +127,95 @@ public class MainActivity extends Activity implements EMDKListener {
     }
 
     @Override
-    public void onOpened(EMDKManager emdkManager) {
-        this.emdkManager = emdkManager;
+    protected void onPause()
+    {
+        super.onPause();
+        deinitSerialComm();
 
-        Log.d(TAG, "EMDK opened");
+        serialCommManager = null;
+        supportedPorts = null;
+        // Release the serialComm manager resources
+        if (emdkManager != null) {
+            emdkManager.release(FEATURE_TYPE.SERIALCOMM_EX);
+        }
+    }
 
-        try{
-            serialComm = (SerialComm) this.emdkManager.getInstance(FEATURE_TYPE.SERIALCOMM);
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
 
-            Thread readThread = new Thread(new Runnable() {
-                @Override
-                public void run() {
+        // Acquire the serialComm manager resources
+        if (emdkManager != null) {
+            serialCommManager = (SerialCommManager) emdkManager.getInstance(FEATURE_TYPE.SERIALCOMM_EX);
 
-                    String statusText = "";
-                    if (serialComm != null) {
-                        try{
-                            serialComm.enable();
-                            statusText = "Serial comm channel enabled";
+            if (serialCommManager != null) {
+                populatePorts();
+                initSerialComm();
+            }
+        }
 
-                            setEnabled(true);
 
-                        } catch(SerialCommException e){
-                            Log.d(TAG, e.getMessage());
-                            statusText = e.getMessage();
-                            setEnabled(false);
-                        }
-                    } else {
-                        statusText = FEATURE_TYPE.SERIALCOMM.toString() + " "+"Feature not supported or initilization error.";
-                        setEnabled(false);
+    }
+
+
+    void populatePorts()
+    {
+        try {
+
+            if(serialCommManager != null) {
+                List<SerialPortInfo> serialPorts = serialCommManager.getSupportedPorts();
+                if(serialPorts.size()>0) {
+                    supportedPorts = new HashMap<String, SerialPortInfo> ();
+                    String[] ports = new String[serialPorts.size()];
+                    int count = 0;
+                    for (SerialPortInfo info : serialPorts) {
+                        supportedPorts.put(info.getFriendlyName(), info);
+                        ports[count] = info.getFriendlyName();
+                        count++;
                     }
-                    displayMessage(statusText);
+
+                    spinnerPorts.setAdapter(new ArrayAdapter<String>(this,
+                            android.R.layout.simple_spinner_dropdown_item, ports));
+
+                    spinnerPorts.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                            //Disabling previous serial port before getting the new one
+                            deinitSerialComm();
+                            initSerialComm();
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> adapterView) {
+
+                        }
+                    });
+
                 }
+                else
+                {
+                    new AsyncStatusUpdate().execute("Failed to get available ports");
+                }
+            }
+            else
+            {
+                new AsyncStatusUpdate().execute("SerialCommManager is null");
+            }
 
-            });
-            readThread.start();
         }
-        catch(Exception e)
+        catch (Exception ex)
         {
-            Log.d(TAG, e.getMessage());
-
-            displayMessage(e.getMessage());
+            Log.d(TAG, ex.getMessage());
+            new AsyncStatusUpdate().execute(ex.getMessage());
         }
+    }
+
+
+
+    void initSerialComm() {
+        new AsyncEnableSerialComm().execute(supportedPorts.get(spinnerPorts.getSelectedItem()));
     }
 
     @Override
@@ -138,89 +223,161 @@ public class MainActivity extends Activity implements EMDKListener {
         if(emdkManager != null) {
             emdkManager.release();
         }
-        displayMessage("EMDK closed abruptly.");
+        new AsyncStatusUpdate().execute("EMDK closed unexpectedly! Please close and restart the application.");
     }
 
-    private void addReadButtonEvents() {
-        readButton = (Button) findViewById(R.id.ReadButton);
-        readButton.setOnClickListener(new OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
+    public void btnReadOnClick(View arg)
+    {
+        new AsyncReadData().execute();
+    }
 
-                Thread readThread = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
+    public void btnWriteOnClick(View arg)
+    {
+        new AsyncUiControlUpdate().execute(false);
+        try {
+            String writeData = txtDataToSend.getText().toString();
+            int bytesWritten = serialCommPort.write(writeData.getBytes(), writeData.getBytes().length);
+            new AsyncStatusUpdate().execute("Bytes written: "+ bytesWritten);
 
-                        setEnabled(false);
-                        String statusText = "";
-                        try {
+        } catch (SerialCommException e) {
+            new AsyncStatusUpdate().execute("write: "+ e.getResult().getDescription());
+        }
+        catch (Exception e) {
+            new AsyncStatusUpdate().execute("write: "+ e.getMessage() + "\n");
+        }
+        new AsyncUiControlUpdate().execute(true);
+    }
 
-                            byte[] readBuffer = serialComm.read(10000); //Timeout after 10 seconds
 
-                            if(readBuffer != null) {
-                                String tempString = new String(readBuffer);
-                                statusText = "Data Read:\n" + tempString;
-                            } else {
-                                statusText = "No Data Available";
-                            }
+    void deinitSerialComm() {
 
-                        }catch (SerialCommException e) {
-                            statusText ="read:"+ e.getResult().getDescription();
-                        }
-                        catch (Exception e) {
-                            statusText = "read:"+ e.getMessage();
-                        }
-                        setEnabled(true);
-                        displayMessage(statusText);
-                    }
-                });
-                readThread.start();
+        if (serialCommPort != null) {
+            try {
+
+                serialCommPort.disable();
+                serialCommPort = null;
+
+            } catch (Exception ex) {
+                Log.d(TAG, "deinitSerialComm disable Exception: " + ex.getMessage());
             }
-        });
+        }
     }
 
-    private void writeButtonEvents() {
-        writeButton = (Button) findViewById(R.id.WriteButton);
+    private class AsyncStatusUpdate extends AsyncTask<String, Void, String> {
 
-        writeButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setEnabled(false);
+        @Override
+        protected String doInBackground(String... params) {
+
+            return params[0];
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            txtStatus.setText(result);
+
+        }
+    }
+
+    private class AsyncUiControlUpdate extends AsyncTask<Boolean, Void, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(Boolean... arg0) {
+
+            return arg0[0];
+        }
+
+        @Override
+        protected void onPostExecute(Boolean bEnable) {
+
+            btnRead.setEnabled(bEnable);
+            btnWrite.setEnabled(bEnable);
+            txtDataToSend.setEnabled(bEnable);
+            spinnerPorts.setEnabled(bEnable);
+        }
+    }
+
+    private class AsyncEnableSerialComm extends AsyncTask<SerialPortInfo, Void, SerialCommResults>
+    {
+        @Override
+        protected SerialCommResults doInBackground(SerialPortInfo... params) {
+
+            SerialCommResults returnvar = SerialCommResults.FAILURE;
+            try {
+                serialCommPort = serialCommManager.getPort(params[0]);
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+            if (serialCommPort != null) {
                 try {
-                    String writeData = editText.getText().toString();
-                    int bytesWritten = serialComm.write(writeData.getBytes(), writeData.getBytes().length);
-                    statusView.setText("Bytes written: "+ bytesWritten);
+                    serialCommPort.enable();
+                    returnvar = SerialCommResults.SUCCESS;
+
                 } catch (SerialCommException e) {
-                    statusView.setText("write: "+ e.getResult().getDescription());
+                    Log.d(TAG, e.getMessage());
+                    e.printStackTrace();
+                    returnvar = e.getResult();
                 }
-                catch (Exception e) {
-                    statusView.setText("write: "+ e.getMessage() + "\n");
-                }
-                setEnabled(true);
             }
-        });
+
+            return returnvar;
+        }
+
+        @Override
+        protected void onPostExecute(SerialCommResults result) {
+            super.onPostExecute(result);
+            if (result == SerialCommResults.SUCCESS) {
+                new AsyncStatusUpdate().execute("Serial comm channel enabled: (" + spinnerPorts.getSelectedItem().toString() + ")");
+                txtDataToSend.setText("Serial Communication Write Data Testing " + spinnerPorts.getSelectedItem().toString() + ".");
+                new AsyncUiControlUpdate().execute(true);
+            } else {
+                new AsyncStatusUpdate().execute(result.getDescription());
+                new AsyncUiControlUpdate().execute(false);
+            }
+        }
     }
 
-    void displayMessage(String message) {
+    private class AsyncReadData extends AsyncTask<Void, Void, String>
+    {
 
-        final String tempMessage = message;
-        runOnUiThread(new Runnable() {
-            public void run() {
-                statusView.setText(tempMessage + "\n");
-            }
-        });
-    }
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            new AsyncUiControlUpdate().execute(false);
+            new AsyncStatusUpdate().execute("Reading..");
+        }
 
-    void setEnabled(boolean enableState) {
+        @Override
+        protected String doInBackground(Void... params) {
+            String statusText = "";
+                try {
 
-        final boolean tempState = enableState;
-        runOnUiThread(new Runnable() {
-            public void run() {
-                readButton.setEnabled(tempState);
-                writeButton.setEnabled(tempState);
-                editText.setEnabled(tempState);
-            }
-        });
+                    byte[] readBuffer = serialCommPort.read(10000); //Timeout after 10 seconds
+
+                    if (readBuffer != null) {
+                        String tempString = new String(readBuffer);
+                        statusText = "Data Read:\n" + tempString;
+                    } else {
+                        statusText = "No Data Available";
+                    }
+
+                } catch (SerialCommException e) {
+                    statusText = "read:" + e.getResult().getDescription();
+                } catch (Exception e) {
+                    statusText = "read:" + e.getMessage();
+                }
+            return statusText;
+        }
+
+        @Override
+        protected void onPostExecute(String statusText) {
+            super.onPostExecute(statusText);
+            new AsyncUiControlUpdate().execute(true);
+            new AsyncStatusUpdate().execute(statusText);
+        }
+
     }
 }
